d5bfc85c9cb83092a3b787c983a08a218f0064b9
Kornel <kornel.tlaczala@gmail.com>
2025-03-17 03:38:16 +0100

    add other operations

--- image_processors.py
+++ image_processors.py
@@ -19,6 +19,10 @@ class ProcessorFlow:
             img = processor.process(img)
         return img
 
+    def reset_cache(self):
+        for processor in self.processors:
+            processor.changed_params = True
+
 
 class Processor:
     def __init__(self):
@@ -26,6 +30,7 @@ class Processor:
         self.last_img = None
 
     def process(self, img):
+        self.changed_params = False
         img_arr = np.array(img)
         img_arr = self._process(img_arr)
         self.last_img = Image.fromarray(img_arr.astype(np.uint8))
@@ -47,26 +52,6 @@ class Processor:
         return self.last_img
 
 
-class GrayscaleProcessor(Processor):
-
-    def __init__(self,):
-        super().__init__()
-        self.default_is_enabled = False
-        self._is_enabled = self.default_is_enabled
-         
-    def _process(self, img_arr):
-        self.changed_params = False
-        if not self._is_enabled:
-            return img_arr
-        return np.dot(img_arr[..., :3], [0.2989, 0.5870, 0.1140])  
-
-    def _process_pixelwise(self, img_arr):
-        return img_arr
-
-    @property
-    def is_enabled(self):
-        return self._is_enabled
-
 class BrightnessProcessor(Processor):
 
     def __init__(self):
@@ -75,14 +60,140 @@ class BrightnessProcessor(Processor):
         self._value = self.default_value
 
     def _process(self, img_arr):
-        self.changed_params = False
         img_arr = np.array(img_arr, dtype=np.int16)
         img_arr = np.clip(img_arr + self._value, 0, 255)
         return img_arr
 
     def _process_pixelwise(self, img_arr):  
-        return img_arr
+        raise NotImplementedError
 
     @property
     def value(self):
         return self._value
+
+
+class ExposureProcessor(Processor):
+
+    def __init__(self):
+        super().__init__()
+        self.default_factor = 1.0
+        self._factor = self.default_factor
+
+    def _process(self, img_arr):
+        img_arr = np.array(img_arr, dtype=np.int16)
+        img_arr = np.clip(img_arr * self._factor, 0, 255)
+        return img_arr
+
+    def _process_pixelwise(self, img_arr):  
+        raise NotImplementedError
+    
+    @property
+    def factor(self):
+        return self._factor
+
+
+class ContrastProcessor(Processor):
+
+    def __init__(self):
+        super().__init__()
+        self.default_factor = 1.0
+        self._factor = self.default_factor
+
+    def _process(self, img_arr):
+        img_arr = np.array(img_arr, dtype=np.int16)
+        mean = np.mean(img_arr)
+        img_arr = np.clip((img_arr - mean) * self._factor + mean, 0, 255)
+        return img_arr
+
+    def _process_pixelwise(self, img_arr):  
+        raise NotImplementedError
+    
+    @property
+    def factor(self):
+        return self._factor
+    
+
+class GammaProcessor(Processor):
+
+    def __init__(self):
+        super().__init__()
+        self.default_factor = 1.0
+        self._factor = self.default_factor
+
+    def _process(self, img_arr):
+        img_arr = np.array(img_arr, dtype=np.int16)
+        img_arr = np.power(img_arr / 255.0, self._factor) * 255
+        return img_arr
+
+    def _process_pixelwise(self, img_arr):  
+        raise NotImplementedError
+    
+    @property
+    def factor(self):
+        return self._factor
+
+
+class GrayscaleProcessor(Processor):
+
+    def __init__(self,):
+        super().__init__()
+        self.default_is_enabled = False
+        self._is_enabled = self.default_is_enabled
+         
+    def _process(self, img_arr):
+        if not self._is_enabled:
+            return img_arr
+        img_arr = np.dot(img_arr[..., :3], [0.2989, 0.5870, 0.1140])  
+        return img_arr
+
+    def _process_pixelwise(self, img_arr):
+        raise NotImplementedError
+
+    @property
+    def is_enabled(self):
+        return self._is_enabled
+
+
+class NegativeProcessor(Processor):
+
+    def __init__(self):
+        super().__init__()
+        self.default_is_enabled = False
+        self._is_enabled = self.default_is_enabled
+
+    def _process(self, img_arr):
+        if not self._is_enabled:
+            return img_arr
+        img_arr = 255 - img_arr
+        return img_arr
+    
+    def _process_pixelwise(self, img_arr):  
+        raise NotImplementedError
+    
+    @property
+    def is_enabled(self):
+        return self._is_enabled
+
+
+class BinarizationProcessor(Processor):
+
+    def __init__(self):
+        super().__init__()
+        self.default_is_enabled = False
+        self._is_enabled = self.default_is_enabled
+        self.default_threshold = 128
+        self._threshold = self.default_threshold
+
+    def _process(self, img_arr):
+        if not self._is_enabled:
+            return img_arr
+        img_arr = np.array(img_arr, dtype=np.int16)
+        img_arr = np.where(img_arr > self._threshold, 255, 0)
+        return img_arr
+
+    def _process_pixelwise(self, img_arr):  
+        raise NotImplementedError
+    
+    @property
+    def threshold(self):
+        return self._threshold
--- new_app.py
+++ new_app.py
@@ -10,29 +10,63 @@ st.title("Image Processing App")
 
 if "processor_flow" not in st.session_state:
     st.session_state.processor_flow = ProcessorFlow()
-    st.session_state.grayscale_processor = GrayscaleProcessor()
     st.session_state.brightness_processor = BrightnessProcessor()
-    st.session_state.processor_flow.add_processor(st.session_state.grayscale_processor)
-    st.session_state.processor_flow.add_processor(st.session_state.brightness_processor)
+    st.session_state.exposure_processor = ExposureProcessor()
+    st.session_state.contrast_processor = ContrastProcessor()
+    st.session_state.gamma_processor = GammaProcessor()
+    st.session_state.grayscale_processor = GrayscaleProcessor()
+    st.session_state.negative_processor = NegativeProcessor()
+    st.session_state.binarization_processor = BinarizationProcessor()
 
+    st.session_state.processor_flow.add_processor(st.session_state.brightness_processor)
+    st.session_state.processor_flow.add_processor(st.session_state.exposure_processor)
+    st.session_state.processor_flow.add_processor(st.session_state.contrast_processor)
+    st.session_state.processor_flow.add_processor(st.session_state.gamma_processor)
+    st.session_state.processor_flow.add_processor(st.session_state.grayscale_processor)
+    st.session_state.processor_flow.add_processor(st.session_state.negative_processor)
+    st.session_state.processor_flow.add_processor(st.session_state.binarization_processor)
+    
 uploaded_file = st.file_uploader("Choose file", type=["jpg", "png", "jpeg"])
+try:
+    if st.session_state.uploaded_file != uploaded_file:
+        st.session_state.processor_flow.reset_cache()
+except:
+    pass
+st.session_state.uploaded_file = uploaded_file
 
 if uploaded_file is not None:
+
     img = Image.open(uploaded_file)
 
     st.sidebar.title("Image Operations")
     st.sidebar.subheader("Pixel Operations")
 
     processor_flow = st.session_state.processor_flow
-    grayscale_processor = st.session_state.grayscale_processor
+    exposure_processor = st.session_state.exposure_processor
     brightness_processor = st.session_state.brightness_processor
+    contrast_processor = st.session_state.contrast_processor
+    gamma_processor = st.session_state.gamma_processor
+    grayscale_processor = st.session_state.grayscale_processor
+    negative_processor = st.session_state.negative_processor
+    binarization_processor = st.session_state.binarization_processor
 
-    grayscale = st.sidebar.checkbox("Grayscale", value=grayscale_processor.default_is_enabled)
+    exposure_factor = st.sidebar.slider("Exposure", 0.1, 3.0, exposure_processor.default_factor)
     brightness_value = st.sidebar.slider("Brightness", -255, 255, brightness_processor.default_value)
+    contrast_factor = st.sidebar.slider("Contrast", 0.1, 3.0, contrast_processor.default_factor)
+    gamma_factor = st.sidebar.slider("Gamma", 0.1, 3.0, gamma_processor.default_factor)
+    grayscale = st.sidebar.checkbox("Grayscale", value=grayscale_processor.default_is_enabled)
+    negative = st.sidebar.checkbox("Negative", value=negative_processor.default_is_enabled)
+    binarization = st.sidebar.checkbox("Binarization", value=binarization_processor.default_is_enabled)
+    binarization_threshold = st.sidebar.slider("Binarization Threshold", 0, 255, binarization_processor.default_threshold)
 
-    grayscale_processor.set_param("_is_enabled", grayscale)
+    exposure_processor.set_param("_factor", exposure_factor)
     brightness_processor.set_param("_value", brightness_value)
-
+    contrast_processor.set_param("_factor", contrast_factor)
+    gamma_processor.set_param("_factor", gamma_factor)
+    grayscale_processor.set_param("_is_enabled", grayscale)
+    negative_processor.set_param("_is_enabled", negative)
+    binarization_processor.set_param("_is_enabled", binarization)
+    binarization_processor.set_param("_threshold", binarization_threshold)
 
     img_processed = processor_flow.process(img)
 
@@ -46,3 +80,4 @@ if uploaded_file is not None:
     with col2:
         st.subheader("Modified Image")
         show_image(img_processed, title="Modified Image")
+    
